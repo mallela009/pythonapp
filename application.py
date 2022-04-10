741a9fd9f1d94e1a57d7623094cca4a32da8e3bd
mallela009 <mallelaarjuna009@gmail.com>
2022-04-10 22:05:17 +0530

    my first commit

---
+++ application.py
@@ -0,0 +1,9 @@
+application_name = "mallela nagarjuna reddy"
+age = 31
+mother_name = "mallela gurrama"
+application_id = "14314324"
+gender ="male"
+print(application_name)
+print(age)
+print(application_id)
+print(gender)
